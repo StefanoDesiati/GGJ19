03c9cbb73205294771b30db0b6979c946b5612d0
StefanoDesiati <desiati.stefano@gmail.com>
2019-01-26 00:23:13 +0100

    aa

--- GGJ19/Assets/Scripts/nothing.cs
+++ GGJ19/Assets/Scripts/nothing.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class void : MonoBehaviour {
+public class nothing : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
