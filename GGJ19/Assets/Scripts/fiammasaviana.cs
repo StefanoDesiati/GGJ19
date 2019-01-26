6a4e5f68b1bc68cf66a4b5589946d0a79c06928f
Simone <lemmasim98@gmail.com>
2019-01-26 04:11:41 +0100

    b

---
+++ GGJ19/Assets/Scripts/fiammasaviana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fiammasaviana : MonoBehaviour {
+	float time = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		transform.position += new Vector3(-0.1f, 0, 0);
+		if(time<0){
+			Destroy(gameObject);
+		}
+	}
+}
