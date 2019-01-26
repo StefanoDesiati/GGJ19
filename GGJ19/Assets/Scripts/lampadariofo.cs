135349f1eb9551420405ea4c3f306cb342a78f6c
Simone <lemmasim98@gmail.com>
2019-01-26 10:58:09 +0100

    provia

---
+++ GGJ19/Assets/Scripts/lampadariofo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lampadariofo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")){
+			this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
+			this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+			//this.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+		}
+	}
+}
