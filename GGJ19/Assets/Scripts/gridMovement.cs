01034faa58180033b983a3230d563923ae879448
StefanoDesiati <desiati.stefano@gmail.com>
2019-01-26 12:22:36 +0100

    inizio movimento a griglia

---
+++ GGJ19/Assets/Scripts/gridMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridMovement : MonoBehaviour {
+	public float speed = 2.0f;
+	public float distance = 1;
+	Vector3 pos;
+
+
+	Transform tr;
+	void Start () {
+		pos = transform.position;
+     	tr = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal")>0.8f && tr.position == pos ) {
+		
+         pos += Vector3.right * distance;
+     }
+     else if (Input.GetAxis("Horizontal")<-0.8f && tr.position == pos) {
+         pos += Vector3.left * distance;
+     }
+     else if (Input.GetAxis("Vertical")>0.8f && tr.position == pos) {
+         pos += Vector3.up * distance;
+     }
+     else if (Input.GetAxis("Vertical")<-0.8f && tr.position == pos) {
+         pos += Vector3.down * distance;
+     }
+     
+     transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+ }   
+	}
+
